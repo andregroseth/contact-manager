0331cd1e33a4a245bb91516cc44695fff6dd0c11
andre groseth <andregroseth@hotmail.com>
2020-01-22 09:15:44 +0100

    chapter 3 JSX Exspressions [done]

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>The App Component</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>The App Component</h1>
+      </div>
+    );
+  }
 }
-
 export default App;
