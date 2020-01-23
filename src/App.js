92b5c8092c9b78f6a09797a7be8d277a01f39202
andre groseth <andregroseth@hotmail.com>
2020-01-23 08:20:09 +0100

    chapter 3 Creating Your First Component [done]

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-import { render } from '@testing-library/react';
+import Contact from './components/Contact';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>The App Component</h1>
+        <Contact />
       </div>
     );
   }
 }
+
 export default App;
