ff904c8292f6c8c74d26f2f05e47f33a97db34c4
andre groseth <andregroseth@hotmail.com>
2020-02-05 08:19:10 +0100

    chapter4: Adding A Context Reducer For Actions [Done]

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react'
-import Contact from './Contact'
-import { Consumer } from '../context'
+import React, {Component} from 'react';
+import Contact from './Contact';
+import {Consumer} from '../context';
 
 class Contacts extends Component {
-  deleteContact = (id) => {
-    const { contacts } = this.state
+    deleteContact = (id) => {
+        const {contacts} = this.state;
 
-    const newContacts = contacts.filter(contact => contact.id !== id);
+        const newContacts = contacts.filter((contact) => contact.id !== id);
 
-    this.setState({
-      contacts: newContacts
-    });
-  }
+        this.setState({
+            contacts: newContacts
+        });
+    };
 
-  render() {
-    return (
-      <Consumer>
-        {value => {
-          const { contacts } = value;
-          return (
-            <React.Fragment>
-              {contacts.map(contact => (
-                <Contact
-                  key={contact.id}
-                  contact={contact}
-                  deleteClickHandler={this.deleteContact.bind(this, contact.id)}
-                />
-              ))}
-            </React.Fragment>
-          )
-        }}
-      </Consumer>
-    )
-  }
+    render() {
+        return (
+            <Consumer>
+                {(value) => {
+                    const {contacts} = value;
+                    return (
+                        <React.Fragment>
+                            {contacts.map((contact) => (
+                                <Contact key={contact.id} contact={contact} />
+                            ))}
+                        </React.Fragment>
+                    );
+                }}
+            </Consumer>
+        );
+    }
 }
-
 
 export default Contacts;
