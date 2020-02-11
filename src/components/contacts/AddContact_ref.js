8d681962fb0ed635a6f14fef8d9054bef74e8981
andre groseth <andregroseth@hotmail.com>
2020-02-11 08:16:32 +0100

    Chapter 5: Form Submit Action To Context [Done]

--- src/components/contacts/AddContact_ref.js
+++ src/components/contacts/AddContact_ref.js
@@ -1,34 +1,33 @@
 import React, {Component} from 'react';
 
 class AddContact extends Component {
-    constructor(props){
-      super(props);
+    constructor(props) {
+        super(props);
 
-      this.nameInput = React.createRef();
-      this.emailInput = React.createRef();
-      this.phoneInput = React.createRef();
+        this.nameInput = React.createRef();
+        this.emailInput = React.createRef();
+        this.phoneInput = React.createRef();
     }
 
     onSubmit = (e) => {
         e.preventDefault();
         const contact = {
-          name: this.nameInput.current.value,
-          email: this.emailInput.current.value,
-          phone: this.phoneInput.current.value
+            name: this.nameInput.current.value,
+            email: this.emailInput.current.value,
+            phone: this.phoneInput.current.value
         };
-        
-        console.log(contact);
 
+        console.log(contact);
     };
 
     static defaultProps = {
-      name: 'Fred Smith',
-      email: 'fred@yahoo.com',
-      phone: '777-777-7777' 
-    }
+        name: 'Fred Smith',
+        email: 'fred@yahoo.com',
+        phone: '777-777-7777'
+    };
 
     render() {
-       const {name, email, phone } =this.props;
+        const {name, email, phone} = this.props;
         return (
             <div className="card mb-3">
                 <div className="card-header">Add Contact</div>
@@ -43,7 +42,7 @@ class AddContact extends Component {
                                 placeholder="Enter Name..."
                                 defaultValue={name}
                                 ref={this.nameInput}
-                                />
+                            />
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
@@ -67,10 +66,7 @@ class AddContact extends Component {
                                 ref={this.phoneInput}
                             />
                         </div>
-                        <input 
-                        type="submit"
-                        value="Add Contact"
-                        className="btn btn-light btn-block" />
+                        <input type="submit" value="Add Contact" className="btn btn-light btn-block" />
                     </form>
                 </div>
             </div>
