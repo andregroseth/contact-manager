72c23fd93647041757bad35f072e7ca230aa0091
andre groseth <andregroseth@hotmail.com>
2020-02-24 08:20:11 +0100

    run prettier

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Contact from './Contact';
-import {Consumer} from '../../context';
+import { Consumer } from '../../context';
 
 class Contacts extends Component {
     deleteContact = (id) => {
-        const {contacts} = this.state;
+        const { contacts } = this.state;
 
         const newContacts = contacts.filter((contact) => contact.id !== id);
 
@@ -17,7 +17,7 @@ class Contacts extends Component {
         return (
             <Consumer>
                 {(value) => {
-                    const {contacts} = value;
+                    const { contacts } = value;
                     return (
                         <React.Fragment>
                             <h1 className="display-4 mb-2">
