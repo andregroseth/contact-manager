a18214d73e806c445262ece7cbbfe3a3d28fe140
andre groseth <andregroseth@hotmail.com>
2020-02-26 07:49:45 +0100

    Chapter 7: Lifecycle, Http & Deployment

---
+++ src/components/test/Test.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+ class Test extends Component {
+  state = {
+    test: 'test'
+  }
+
+
+  componentDidMount() {
+    console.log('componentDidMount...');
+  }
+
+  UNSAFE_componentWillMount() {
+    console.log('componentWillMount...');
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate...');
+  }
+
+  UNSAFE_componentWillUpdate() {
+    console.log('componentWillUpdate...');
+  }
+
+  UNSAFE_componentWillReceiveProps(nextProps, nextState){
+    console.log('componentWillReceiveProps...');
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState){
+    return {
+      test: 'something'
+    };
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('getSnapshotBeforeUpdate...');
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Test Component</h1>
+      </div>
+    )
+  }
+}
+
+export default Test;
