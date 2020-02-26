5bbe73e3bb17855bcb977e9af9c2bb7d1831922a
andre groseth <andregroseth@hotmail.com>
2020-02-26 09:03:41 +0100

    Chapter 7: POST & DELETE Requests [done]

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react'
 
- class Test extends Component {
+class Test extends Component {
   state = {
-    test: 'test'
+    title: '',
+    id: '',
+    userId: '',
+    completed: ''
   }
 
 
   componentDidMount() {
-    console.log('componentDidMount...');
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+      .then(response => response.json())
+      .then(data => this.setState({
+        title: data.title,
+        body: data.body,
+      })
+      );
   }
 
-  UNSAFE_componentWillMount() {
-    console.log('componentWillMount...');
-  }
+  // UNSAFE_componentWillMount() {
+  //   console.log('componentWillMount...');
+  // }
 
-  componentDidUpdate() {
-    console.log('componentDidUpdate...');
-  }
+  // componentDidUpdate() {
+  //   console.log('componentDidUpdate...');
+  // }
 
-  UNSAFE_componentWillUpdate() {
-    console.log('componentWillUpdate...');
-  }
+  // UNSAFE_componentWillUpdate() {
+  //   console.log('componentWillUpdate...');
+  // }
 
-  UNSAFE_componentWillReceiveProps(nextProps, nextState){
-    console.log('componentWillReceiveProps...');
-  }
+  // UNSAFE_componentWillReceiveProps(nextProps, nextState){
+  //   console.log('componentWillReceiveProps...');
+  // }
 
-  static getDerivedStateFromProps(nextProps, prevState){
-    return {
-      test: 'something'
-    };
-  }
+  // static getDerivedStateFromProps(nextProps, prevState){
+  //   return {
+  //     test: 'something'
+  //   };
+  // }
 
-  getSnapshotBeforeUpdate(prevProps, prevState){
-    console.log('getSnapshotBeforeUpdate...');
-  }
+  // getSnapshotBeforeUpdate(prevProps, prevState){
+  //   console.log('getSnapshotBeforeUpdate...');
+  // }
 
   render() {
+    const { title, body,} = this.state;
     return (
       <div>
         <h1>Test Component</h1>
+        <h1>Test title: {title}</h1>
+        <p>Test body: {body}</p>
       </div>
     )
   }
