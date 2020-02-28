538d60859ce67f0f861c66e76cd5ca6becbeb362
andre groseth <andregroseth@hotmail.com>
2020-02-28 08:31:13 +0100

    Chapter 7: EditContact Component & Fetch Contact [done]

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import {Consumer} from '../../context';
+import { Consumer } from '../../context';
 import axios from 'axios';
 
 class Contact extends Component {
@@ -8,25 +9,25 @@ class Contact extends Component {
         showContactInfo: false
     };
 
-    onDeleteClick = async(id, dispatch) => {
-        try{
-        await axios.delete
-          (`https://jsonplaceholder.typicode.com/users/${id}`)
+    onDeleteClick = async (id, dispatch) => {
+        try {
+            await axios.delete
+                (`https://jsonplaceholder.typicode.com/users/${id}`)
 
-          dispatch({ type: 'DELETE_CONTACT', payload: id });
-        } catch(e) {
-          dispatch({ type: 'DELETE_CONTACT', payload: id });
+            dispatch({ type: 'DELETE_CONTACT', payload: id });
+        } catch (e) {
+            dispatch({ type: 'DELETE_CONTACT', payload: id });
         }
     };
 
     render() {
-        const {id, name, email, phone} = this.props.contact;
-        const {showContactInfo} = this.state;
+        const { id, name, email, phone } = this.props.contact;
+        const { showContactInfo } = this.state;
 
         return (
             <Consumer>
                 {(value) => {
-                    const {dispatch} = value;
+                    const { dispatch } = value;
                     return (
                         <div className="card card-body mb-3">
                             <h4>
@@ -38,13 +39,24 @@ class Contact extends Component {
                                         })
                                     }
                                     className="fas fa-sort-down"
-                                    style={{cursor: 'pointer'}}
+                                    style={{ cursor: 'pointer' }}
                                 />
                                 <i
                                     className="fas fa-times"
-                                    style={{cursor: 'pointer', float: 'right', color: 'red'}}
+                                    style={{ cursor: 'pointer', float: 'right', color: 'red' }}
                                     onClick={this.onDeleteClick.bind(this, id, dispatch)}
                                 />
+                                <Link to={`contact/edit/${id}`}>
+                                    <i
+                                        className="fas fa-pencil-alt"
+                                        style={{
+                                            cursor: 'pointer',
+                                            float: 'right',
+                                            color: 'black',
+                                            marginRight: '1rem'
+                                        }}
+                                    />
+                                </Link>
                             </h4>
                             {showContactInfo ? (
                                 <ul className="list-group">
