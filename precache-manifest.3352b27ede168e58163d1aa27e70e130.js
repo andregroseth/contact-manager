dcead081b4f63e7025fea5a035a92ddbc144359c
andre groseth <andregroseth@hotmail.com>
2020-03-04 08:31:59 +0100

    Updates

---
+++ precache-manifest.3352b27ede168e58163d1aa27e70e130.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be6dc6fb31b4f8429332d5cbbd9a1701",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "dcde69a39365f46ab35d",
+    "url": "/contact-manager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "57bd10f16bc1dda01d11",
+    "url": "/contact-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dcde69a39365f46ab35d",
+    "url": "/contact-manager/static/js/2.1e7688d1.chunk.js"
+  },
+  {
+    "revision": "dd793fd0cebf405635f3106ffdc89e70",
+    "url": "/contact-manager/static/js/2.1e7688d1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "57bd10f16bc1dda01d11",
+    "url": "/contact-manager/static/js/main.10936921.chunk.js"
+  },
+  {
+    "revision": "8a32f839e8d8e639c5b9",
+    "url": "/contact-manager/static/js/runtime-main.e44b73f7.js"
+  }
+]);
